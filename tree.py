f1e7e3a0123f1fc7239f350dffd6df4f12cbd7d7
saikeerthana-224 <kotni.keerthana2004@gmail.com>
2024-07-23 13:36:35 +0530

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,16 +1,21 @@
-class node:
+class Node:
   def __init__(self,data):
     self.data=data
-    self.left=left
-    self.right=right
-  def preorder(self,root):
-    if(root==none):
+    self.left=None
+    self.right=None
+  def preOrder(self,root):
+    if(root==None):
       return
     print(root.data,end=" ")
-    root.preorder(root.left)
-    root.preorder(root.right)
-root=node(1)
-root.left=node(2)
-root.right=node(3)
+    root.preOrder(root.left)
+    root.preOrder(root.right)
+root=Node(1)
+root.left=Node(2)
+root.right=Node(3)
+root.left.left=Node(4)
+root.left.right=Node(5)
+root.right.left=Node(6)
+root.right.right=Node(7)
+root.preOrder(root)
 
     
